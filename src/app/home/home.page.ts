a54779fab19c0f5dec2703fcce41d1accd10f560
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-10-21 17:07:25 +0200

    prepare prime numbers

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,56 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+
+
   constructor() {}
+
+
+
+  parcours(): void {
+
+    const names: string [] = ["john","sara"];
+
+      for (let index = 0; index < names.length; index++) {
+        const unNom = names[index];
+        console.log(unNom);
+      }
+  }
+
+  /**
+   * 
+   * @param n 
+   * @returns 
+   */
+  generate(n: number):boolean [] {
+    return null; 
+  }
+
+  /**
+   * 
+   * @param n 
+   */
+  crible(n: number): boolean [] {
+    return null;
+  }
+
+  /**
+   * 
+   * @param n 
+   */
+  printy(n: number):void {
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
 
 }
